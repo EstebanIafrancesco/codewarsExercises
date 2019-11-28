07ba59ceed332905803fe1f34a8eaa886e1ddf04
Esteban <esteban.iafrancesco@gmail.com>
2019-11-27 20:27:06 -0500

    Commit para guardar proyectos de codewars

---
+++ caracteresRepetidos.js
@@ -0,0 +1,32 @@
+/*
+Write a function that will return the count of distinct case-insensitive alphabetic characters 
+and numeric digits that occur more than once in the input string. The input string can be 
+assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+
+Escriba una función que devuelva el recuento de caracteres alfabéticos distintos de mayúsculas 
+y minúsculas y dígitos numéricos que aparecen más de una vez en la cadena de entrada. Se puede
+suponer que la cadena de entrada contiene solo alfabetos (tanto mayúsculas como minúsculas) y 
+dígitos numéricos.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+*/
+var cadena = "";
+
+
+function caracteresRepetidos(cadena){
+    // medir longitud de la cadena
+    let cadenaLong = cadena.length;
+    for(i=0;i<cadenaLong;i++){
+
+    }
+    return mensaje;
+}
